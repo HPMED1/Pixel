0ad39d0762b7cfebf16edb943e2034bbcadd34e2
HPMED1 <62830536+HPMED1@users.noreply.github.com>
2022-11-30 19:36:11 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,33 +1,92 @@
-#
+#the imports--------------------------------------------------------------------
 import discord
+import requests
 import random
+from discord.ext import commands
 from discord.ext import commands, tasks
-import youtube_dl
-import os
-import time
-import sys
-import DiscordUtils
-#the perfix
+import json
+#the perfix---------------------------------------------------------------------pip
+
 b = commands.Bot(command_prefix='.')
-
-
-
-#b.remove_command("help")
-#events
-#on ready
+#events-------------------------------------------------------------------------
+#on ready-----------------------------------------------------------------------
 @b.event
 async def on_ready():
-    print("Bot online")
-
-
-#commands
-#ping
+    
+    print('bot is here')
+#commands-----------------------------------------------------------------------
+#ready--------------------------------------------------------------------------
+@b.command(aliases=['ready', 'r'])
+async def _ready(ctx):
+    await ctx.send('yes')
+#ping---------------------------------------------------------------------------
 @b.command()
 async def ping(ctx):
     await ctx.send(f'pong! {round(b.latency * 1000)}ms')
 
+@b.command()
+async def Affirmation(ctx):
+    r = requests.get("https://www.affirmations.dev")
+    embed = discord.Embed(color=0x000000)
+    embed.add_field(name="Answer", value=r.json()['affirmation'], inline=True)
+    await ctx.send(embed=embed)
 
-#8ball
+
+@b.command()
+async def Advice(ctx):
+    r = requests.get("https://api.adviceslip.com/advice")
+    embed = discord.Embed(color=0x000000)
+    embed.add_field(name="Answer", value=r.json()['slip']['advice'], inline=True)
+    await ctx.send(embed=embed)
+
+
+@b.command()
+async def Evilinsult(ctx):
+    r = requests.get(f"https://evilinsult.com/generate_insult.php?lang=en&numbe={random.randint(1,3000)}&type=json")
+    
+    embed = discord.Embed(color=0x000000)
+    embed.add_field(name="Answer", value=r.json()['insult'], inline=True)
+    await ctx.send(embed=embed)
+@b.command()
+async def Define (ctx, msg):
+    r = requests.get(f"https://api.urbandictionary.com/v0/define?term={msg}")
+    embed = discord.Embed(color=0x000000)
+    embed.add_field(name="Question", value=msg, inline=True)
+    embed.add_field(name="Answer", value=r.json()['list'][random.randrange(0,10)]['definition'], inline=True)
+    await ctx.send(embed=embed)
+
+
+@b.command()
+async def _8Ball(ctx,msg):
+    r = requests.get(f"https://8ball.delegator.com/magic/JSON/{msg}")
+    embed = discord.Embed(color=0x000000)
+    embed.add_field(name="Question", value=msg, inline=True)
+    embed.add_field(name="Answer", value=r.json()['magic']['answer'], inline=True)
+    await ctx.send(embed=embed)
+
+
+#make a new command to that gets info on the user-----------------------------------
+@b.command()
+async def userinfo(ctx, member: discord.Member):
+    embed = discord.Embed(title=f"{member}'s info", color=0x000000)
+    embed.add_field(name="Name", value=member.name, inline=True)
+    embed.add_field(name="ID", value=member.id, inline=True)
+    embed.add_field(name="Status", value=member.status, inline=True)
+    embed.add_field(name="Highest role", value=member.top_role, inline=True)
+    embed.add_field(name="Joined", value=member.joined_at, inline=True)
+    await ctx.send(embed=embed)
+#make a new command to that gets info on the server-----------------------------------
+@b.command()
+async def serverinfo(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}'s info", color=0x000000)
+    embed.add_field(name="Name", value=ctx.guild.name, inline=True)
+    embed.add_field(name="ID", value=ctx.guild.id, inline=True)
+    embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.guild.roles), inline=True)
+    embed.add_field(name="Channels", value=len(ctx.guild.channels), inline=True)
+    await ctx.send(embed=embed)
+
+
 @b.command(aliases=['8ball', '8BALL'],
            help="ask me something and i will answer")
 async def _8ball(ctx, *, question):
@@ -97,117 +156,4 @@ async def say(ctx, ):
 
     await ctx.send(f'{ctx.author.mention} made me say\n{message.content}')
 
-music = DiscordUtils.Music()
-@b.command(aliases=['J', 'j'], help="Joins the voice channel")
-async def join(ctx):
-    await ctx.author.voice.channel.connect()
-    await ctx.send("I'm connected")
-
-
-@b.command(aliases=['l', 'L', 'ds', 'DS'], help="leaves the voice channel")
-async def leave(ctx):
-    await ctx.voice_client.disconnect()
-
-
-@b.command(aliases=['p', 'P'], help="play music")
-async def play(ctx, *, url):
-    if not ctx.guild.voice_client in b.voice_clients:
-        await ctx.author.voice.channel.connect()
-        await ctx.send("I'm connected")
-    time.sleep(0.5)
-    player = music.get_player(guild_id=ctx.guild.id)
-    time.sleep(0.5)
-    if not player:
-        player = music.create_player(ctx, ffmpeg_error_betterfix=True)
-    if not ctx.voice_client.is_playing():
-        await player.queue(url, search=True)
-        song = await player.play()
-        await ctx.send(f"Playing {song.name}")
-    else:
-        song = await player.queue(url, search=True)
-        await ctx.send(f"Queued {song.name}")
-
-
-@b.command(aliases=['wait', 'WAIT'], help="pause the music")
-async def pause(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song = await player.pause()
-    await ctx.send(f"Paused {song.name}")
-
-
-@b.command(aliases=['r', 'R'], help="resume the music")
-async def resume(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song = await player.resume()
-    await ctx.send(f"Resumed {song.name}")
-
-
-@b.command(help="stop the music")
-async def stop(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    await player.stop()
-    await ctx.send("Stopped")
-
-
-@b.command(help="loop the music")
-async def loop(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song = await player.toggle_song_loop()
-    if song.is_looping:
-        await ctx.send(f"Enabled loop for {song.name}")
-    else:
-        await ctx.send(f"Disabled loop for {song.name}")
-
-
-@b.command(aliases=['q', 'Q'], help="see the queue")
-async def queue(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    await ctx.send(
-        f"{', '.join([song.name for song in player.current_queue()])}")
-
-
-@b.command(help="see the name of the song playing right now")
-async def np(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song = player.now_playing()
-    await ctx.send(song.name)
-
-
-@b.command(aliases=['s', 'S'], help="skip songs")
-async def skip(ctx):
-    player = music.get_player(guild_id=ctx.guild.id)
-    data = await player.skip(force=True)
-    if len(data) == 2:
-        await ctx.send(f"Skipped from {data[0].name} to {data[1].name}")
-    else:
-        await ctx.send(f"Skipped {data[0].name}")
-
-
-@b.command(aliases=['vol', 'VOL', 'v', 'V'],
-           help="change the volume of the music")
-async def volume(ctx, vol):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song, volume = await player.change_volume(
-        float(vol) / 100)  # volume should be a float between 0 to 1
-    await ctx.send(f"Changed volume for {song.name} to {volume*100}%")
-
-
-#
-@b.command(help="remove a song in the queue")
-async def remove(ctx, index):
-    player = music.get_player(guild_id=ctx.guild.id)
-    song = await player.remove_from_queue(int(index))
-    await ctx.send(f"Removed {song.name} from queue")
-
-
-@b.command()
-async def add(ctx, num1: int, num2: int):
-    res = float(num1 + num2)
-    await ctx.send(res)
-keep_alive()
-#the run
-@b.command()
-async def servers(ctx):
-  guilds = await b.fetch_guilds(limit=150).flatten()
-  await ctx.send(f'`{guilds}`')
-b.run('NzQxNzg0NjcwMzc4NjU1NzY2.Xy8mzw.WcBlJI7pW2U8rZVizr8xiLg3Guo')
+b.run('NzQxNzg0NjcwMzc4NjU1NzY2.GAbPYj.O2OMdtntmfM3gJ3IvYUkFrFXVQwc-yk8uCVPu8')
