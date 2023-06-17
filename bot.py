ad3cfbd8471bb05938d6bfaca35576891a79156b
HPMED1 <62830536+HPMED1@users.noreply.github.com>
2023-06-17 13:28:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #the imports--------------------------------------------------------------------
 import discord
+from discord import app_commands
 import requests
 import random
 from discord.ext import commands, tasks
@@ -7,138 +8,104 @@ import json
 import os
 #the perfix---------------------------------------------------------------------pip
 
-b = commands.Bot(command_prefix='.')
+intents = discord.Intents.all()
+# if you don't want all intents you can do discord.Intents.default()
+b = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(b)
 #events-------------------------------------------------------------------------
 #on ready-----------------------------------------------------------------------
+
+
 @b.event
 async def on_ready():
-    print('bot is here')
+  await tree.sync(guild=discord.Object(id=871311803630645279))
+  # print "ready" in the console when the bot is ready to work
+  print("ready")
+
+
 #commands-----------------------------------------------------------------------
 #ready--------------------------------------------------------------------------
-@b.slash_command(name="ready", guild_ids=[871311803630645279])
-async def _ready(ctx):
-    await ctx.send('yes')
+@tree.command(name="ready", guild=discord.Object(id=871311803630645279))
+async def _ready(interaction: discord.Interaction):
+
+  await interaction.response.send_message("Hello!")
+
+
 #ping---------------------------------------------------------------------------
-@b.slash_command(name="ping", guild_ids=[871311803630645279])
-async def ping(ctx):
-    await ctx.send(f'pong! {round(b.latency * 1000)}ms')
-
-@b.slash_command(name="affirmaion", guild_ids=[871311803630645279])
-async def affirmation(ctx):
-    r = requests.get("https://www.affirmations.dev")
-    embed = discord.Embed(color=0x000000)
-    embed.add_field(name="Answer", value=r.json()['affirmation'], inline=True)
-    await ctx.send(embed=embed)
+@tree.command(name="ping", guild=discord.Object(id=871311803630645279))
+async def ping(interaction: discord.Interaction):
+  await interaction.response.send_message(f'pong! {round(b.latency * 1000)}ms')
 
 
-@b.slash_command(name="advice", guild_ids=[871311803630645279])
-async def advice(ctx):
-    r = requests.get("https://api.adviceslip.com/advice")
-    embed = discord.Embed(color=0x000000)
-    embed.add_field(name="Answer", value=r.json()['slip']['advice'], inline=True)
-    await ctx.send(embed=embed)
+@tree.command(name="affirmaion", guild=discord.Object(id=871311803630645279))
+async def affirmation(interaction: discord.Interaction):
+  r = requests.get("https://www.affirmations.dev")
+  embed = discord.Embed(color=0x000000)
+  embed.add_field(name="Answer", value=r.json()['affirmation'], inline=True)
+  await interaction.response.send_message(embed=embed)
 
 
-@b.slash_command(name="evilinsult", guild_ids=[871311803630645279])
-async def evilinsult(ctx):
-    r = requests.get(f"https://evilinsult.com/generate_insult.php?lang=en&numbe={random.randint(1,3000)}&type=json")
-    
-    embed = discord.Embed(color=0x000000)
-    embed.add_field(name="Answer", value=r.json()['insult'], inline=True)
-    await ctx.send(embed=embed)
-@b.slash_command(name="define", guild_ids=[871311803630645279])
-async def define (ctx, msg):
-    r = requests.get(f"https://api.urbandictionary.com/v0/define?term={msg}")
-    embed = discord.Embed(color=0x000000)
-    embed.add_field(name="Question", value=msg, inline=True)
-    embed.add_field(name="Answer", value=r.json()['list'][random.randrange(0,10)]['definition'], inline=True)
-    await ctx.send(embed=embed)
+@tree.command(name="advice", guild=discord.Object(id=871311803630645279))
+async def advice(interaction: discord.Interaction):
+  r = requests.get("https://api.adviceslip.com/advice")
+  embed = discord.Embed(color=0x000000)
+  embed.add_field(name="Answer", value=r.json()['slip']['advice'], inline=True)
+  await interaction.response.send_message(embed=embed)
 
 
-@b.slash_command(name="8ball", guild_ids=[871311803630645279])
-async def _8ball(ctx,msg):
-    r = requests.get(f"https://8ball.delegator.com/magic/JSON/{msg}")
-    embed = discord.Embed(color=0x000000)
-    embed.add_field(name="Question", value=msg, inline=True)
-    embed.add_field(name="Answer", value=r.json()['magic']['answer'], inline=True)
-    await ctx.send(embed=embed)
+@tree.command(name="evilinsult", guild=discord.Object(id=871311803630645279))
+async def evilinsult(interaction: discord.Interaction):
+  r = requests.get(
+    f"https://evilinsult.com/generate_insult.php?lang=en&numbe={random.randint(1,3000)}&type=json"
+  )
+
+  embed = discord.Embed(color=0x000000)
+  embed.add_field(name="Answer", value=r.json()['insult'], inline=True)
+  await interaction.response.send_message(embed=embed)
 
 
-#make a new command to that gets info on the user-----------------------------------
-@b.slash_command(name="userinfo", guild_ids=[871311803630645279])
-async def userinfo(ctx, member: discord.Member):
-    embed = discord.Embed(title=f"{member}'s info", color=0x000000)
-    embed.add_field(name="Name", value=member.name, inline=True)
-    embed.add_field(name="ID", value=member.id, inline=True)
-    embed.add_field(name="Status", value=member.status, inline=True)
-    embed.add_field(name="Highest role", value=member.top_role, inline=True)
-    embed.add_field(name="Joined", value=member.joined_at, inline=True)
-    await ctx.send(embed=embed)
-#make a new command to that gets info on the server-----------------------------------
-@b.slash_command(name="serverinfo", guild_ids=[871311803630645279])
-async def serverinfo(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}'s info", color=0x000000)
-    embed.add_field(name="Name", value=ctx.guild.name, inline=True)
-    embed.add_field(name="ID", value=ctx.guild.id, inline=True)
-    embed.add_field(name="Owner", value=ctx.guild.owner, inline=True)
-    embed.add_field(name="Roles", value=len(ctx.guild.roles), inline=True)
-    embed.add_field(name="Channels", value=len(ctx.guild.channels), inline=True)
-    await ctx.send(embed=embed)
-
+@tree.command(name="define", guild=discord.Object(id=871311803630645279))
+@app_commands.describe(thing_to_define="what should i define")
+async def define(interaction: discord.Interaction, thing_to_define: str):
+  r = requests.get(
+    f"https://api.urbandictionary.com/v0/define?term={thing_to_define}")
+  embed = discord.Embed(color=0x000000)
+  embed.add_field(name="Question", value=thing_to_define, inline=True)
+  embed.add_field(name="Answer",
+                  value=r.json()['list'][random.randrange(0,
+                                                          10)]['definition'],
+                  inline=True)
+  await interaction.response.send_message(embed=embed)
 
 
 #how much
-@b.slash_command(name="howmuch", guild_ids=[871311803630645279])
-async def how_much(ctx, *, member_name):
-    how_list = [
-        '0%', '1%', '2%', '3%', '4%', '5%', '6%', '7%', '8%', '9%', '10%',
-        '11%', '12%', '13%', '14%', '15%', '16%', '17%', '18%', '19%', '20%',
-        '21%', '22%', '23%', '24%', '25%', '26%', '27%', '28%', '29%', '30%',
-        '31%', '32%', '33%', '34%', '35%', '36%', '37%', '38%', '39%', '40%',
-        '41%', '42%', '43%', '44%', '45%', '46%', '47%', '48%', '49%', '50%',
-        '51%', '52%', '53%', '54%', '55%', '56%', '57%', '58%', '59%', '60%',
-        '61%', '62%', '63%', '64%', '65%', '66%', '67%', '68%', '69%', '70%',
-        '71%', '72%', '73%', '74%', '75%', '76%', '77%', '78%', '79%', '80%',
-        '81%', '82%', '83%', '84%', '85%', '86%', '87%', '88%', '89%', '90%',
-        '91%', '92%', '93%', '94%', '95%', '96%', '97%', '98%', '99%', '100%',
-        'idk'
-    ]
-    await ctx.send(f'{random.choice(how_list)}')
-
-
-#question
-@b.slash_command(name="question", guild_ids=[871311803630645279])
-async def question(ctx, *, question1):
-    does_list = ['no|لا', 'yes|ايوه', 'maybe|ممكن']
-    await ctx.send(f'{random.choice(does_list)}')
-
-
-#dm
-@b.slash_command(name="dm", guild_ids=[871311803630645279])
-async def dm(ctx, member: discord.Member):
-    await ctx.send("what do you want to say")
-
-    def check(m):
-        return m.author.id == ctx.author.id
-
-    message = await b.wait_for('message', check=check)
-
-    await ctx.send(f'the message is sent to {member}')
-
-    await member.send(
-        f'{ctx.author.mention} Has a message for you:\n{message.content}')
+@tree.command(name="howmuch", guild=discord.Object(id=871311803630645279))
+@app_commands.describe(question="question")
+async def how_much(interaction: discord.Interaction, question: str):
+  how_list = [
+    '0%', '1%', '2%', '3%', '4%', '5%', '6%', '7%', '8%', '9%', '10%', '11%',
+    '12%', '13%', '14%', '15%', '16%', '17%', '18%', '19%', '20%', '21%',
+    '22%', '23%', '24%', '25%', '26%', '27%', '28%', '29%', '30%', '31%',
+    '32%', '33%', '34%', '35%', '36%', '37%', '38%', '39%', '40%', '41%',
+    '42%', '43%', '44%', '45%', '46%', '47%', '48%', '49%', '50%', '51%',
+    '52%', '53%', '54%', '55%', '56%', '57%', '58%', '59%', '60%', '61%',
+    '62%', '63%', '64%', '65%', '66%', '67%', '68%', '69%', '70%', '71%',
+    '72%', '73%', '74%', '75%', '76%', '77%', '78%', '79%', '80%', '81%',
+    '82%', '83%', '84%', '85%', '86%', '87%', '88%', '89%', '90%', '91%',
+    '92%', '93%', '94%', '95%', '96%', '97%', '98%', '99%', '100%', 'idk'
+  ]
+  await interaction.response.send_message(f'{random.choice(how_list)}')
 
 
 #say
-@b.slash_command(name="say", guild_ids=[871311803630645279])
-async def say(ctx, ):
-    await ctx.send("what do you want me to say")
+@tree.command(name="sadcat", guild=discord.Object(id=871311803630645279))
+@app_commands.describe(question="question")
+async def advice(interaction: discord.Interaction, question: str):
+  data = question.replace(" ", "+")
+  r = requests.get(f"https://api.popcat.xyz/sadcat?text={data}")
+  with open("myimage.png", "wb") as f:
+    f.write(r.content)
+  await interaction.response.send_message(f'{interaction.user.mention} Made This!', file = discord.File("myimage.png"))
 
-    def check(m):
-        return m.author.id == ctx.author.id
-
-    message = await b.wait_for('message', check=check)
-
-    await ctx.send(f'{ctx.author.mention} made me say\n{message.content}')
 
 b.run("NzQxNzg0NjcwMzc4NjU1NzY2.GuqQKz.8R3jmDmh3CDjYyzl-O2bl6SLGyhMPqb-tU3jo8")
