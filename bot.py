bee1bd09b85d53927d3f5a8a06992da5c7aa183b
HPMED1 <62830536+HPMED1@users.noreply.github.com>
2023-06-17 10:18:20 +0300

    slash commands

--- bot.py
+++ bot.py
@@ -2,14 +2,9 @@
 import discord
 import requests
 import random
-from discord.ext import commands
 from discord.ext import commands, tasks
 import json
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
-TOKEN = os.getenv('token')
 #the perfix---------------------------------------------------------------------pip
 
 b = commands.Bot(command_prefix='.')
@@ -17,43 +12,42 @@ b = commands.Bot(command_prefix='.')
 #on ready-----------------------------------------------------------------------
 @b.event
 async def on_ready():
-    
     print('bot is here')
 #commands-----------------------------------------------------------------------
 #ready--------------------------------------------------------------------------
-@b.command(aliases=['ready', 'r'])
+@b.slash_command(name="ready", guild_ids=[871311803630645279])
 async def _ready(ctx):
     await ctx.send('yes')
 #ping---------------------------------------------------------------------------
-@b.command()
+@b.slash_command(name="ping", guild_ids=[871311803630645279])
 async def ping(ctx):
     await ctx.send(f'pong! {round(b.latency * 1000)}ms')
 
-@b.command()
-async def Affirmation(ctx):
+@b.slash_command(name="affirmaion", guild_ids=[871311803630645279])
+async def affirmation(ctx):
     r = requests.get("https://www.affirmations.dev")
     embed = discord.Embed(color=0x000000)
     embed.add_field(name="Answer", value=r.json()['affirmation'], inline=True)
     await ctx.send(embed=embed)
 
 
-@b.command()
-async def Advice(ctx):
+@b.slash_command(name="advice", guild_ids=[871311803630645279])
+async def advice(ctx):
     r = requests.get("https://api.adviceslip.com/advice")
     embed = discord.Embed(color=0x000000)
     embed.add_field(name="Answer", value=r.json()['slip']['advice'], inline=True)
     await ctx.send(embed=embed)
 
 
-@b.command()
-async def Evilinsult(ctx):
+@b.slash_command(name="evilinsult", guild_ids=[871311803630645279])
+async def evilinsult(ctx):
     r = requests.get(f"https://evilinsult.com/generate_insult.php?lang=en&numbe={random.randint(1,3000)}&type=json")
     
     embed = discord.Embed(color=0x000000)
     embed.add_field(name="Answer", value=r.json()['insult'], inline=True)
     await ctx.send(embed=embed)
-@b.command()
-async def Define (ctx, msg):
+@b.slash_command(name="define", guild_ids=[871311803630645279])
+async def define (ctx, msg):
     r = requests.get(f"https://api.urbandictionary.com/v0/define?term={msg}")
     embed = discord.Embed(color=0x000000)
     embed.add_field(name="Question", value=msg, inline=True)
@@ -61,8 +55,8 @@ async def Define (ctx, msg):
     await ctx.send(embed=embed)
 
 
-@b.command()
-async def _8Ball(ctx,msg):
+@b.slash_command(name="8ball", guild_ids=[871311803630645279])
+async def _8ball(ctx,msg):
     r = requests.get(f"https://8ball.delegator.com/magic/JSON/{msg}")
     embed = discord.Embed(color=0x000000)
     embed.add_field(name="Question", value=msg, inline=True)
@@ -71,7 +65,7 @@ async def _8Ball(ctx,msg):
 
 
 #make a new command to that gets info on the user-----------------------------------
-@b.command()
+@b.slash_command(name="userinfo", guild_ids=[871311803630645279])
 async def userinfo(ctx, member: discord.Member):
     embed = discord.Embed(title=f"{member}'s info", color=0x000000)
     embed.add_field(name="Name", value=member.name, inline=True)
@@ -81,7 +75,7 @@ async def userinfo(ctx, member: discord.Member):
     embed.add_field(name="Joined", value=member.joined_at, inline=True)
     await ctx.send(embed=embed)
 #make a new command to that gets info on the server-----------------------------------
-@b.command()
+@b.slash_command(name="serverinfo", guild_ids=[871311803630645279])
 async def serverinfo(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}'s info", color=0x000000)
     embed.add_field(name="Name", value=ctx.guild.name, inline=True)
@@ -92,15 +86,9 @@ async def serverinfo(ctx):
     await ctx.send(embed=embed)
 
 
-@b.command(aliases=['8ball', '8BALL'],
-           help="ask me something and i will answer")
-async def _8ball(ctx, *, question):
-    rr = ['no|لا', 'yes|ايوه', 'Maybe|ممكن', '5555555555555|خخخخخخخخخخخ']
-    await ctx.send(f'QUESTION:{question}\nAnswer: {random.choice(rr)}')
-
 
 #how much
-@b.command(aliases=['howmuch', 'HOW_MUCH'], help="number%")
+@b.slash_command(name="howmuch", guild_ids=[871311803630645279])
 async def how_much(ctx, *, member_name):
     how_list = [
         '0%', '1%', '2%', '3%', '4%', '5%', '6%', '7%', '8%', '9%', '10%',
@@ -119,22 +107,14 @@ async def how_much(ctx, *, member_name):
 
 
 #question
-@b.command(aliases=[
-    'DOES', 'Does', 'does', 'do', 'Do', 'DO', 'did', 'Did', 'DID', 'will',
-    'Will', 'WILL', 'shall', 'Shall', 'SHALL', 'are', 'Are', 'ARE', 'am', 'Am',
-    'AM', 'have', 'Have', 'HAVE', 'has', 'Has', 'HAS', 'can', 'Can', 'CAN',
-    'must', 'Must', 'MUST', 'might', 'Might', 'MIGHT', 'COULD', 'could',
-    'Could'
-    'is', 'IS', 'Is'
-],
-           help="ask me somthing and i will answer with yes or no")
+@b.slash_command(name="question", guild_ids=[871311803630645279])
 async def question(ctx, *, question1):
     does_list = ['no|لا', 'yes|ايوه', 'maybe|ممكن']
     await ctx.send(f'{random.choice(does_list)}')
 
 
 #dm
-@b.command(aliases=['DM'], help="send a message to someone")
+@b.slash_command(name="dm", guild_ids=[871311803630645279])
 async def dm(ctx, member: discord.Member):
     await ctx.send("what do you want to say")
 
@@ -150,7 +130,7 @@ async def dm(ctx, member: discord.Member):
 
 
 #say
-@b.command(aliases=['SAY'], help="do i have to explain?")
+@b.slash_command(name="say", guild_ids=[871311803630645279])
 async def say(ctx, ):
     await ctx.send("what do you want me to say")
 
@@ -161,4 +141,4 @@ async def say(ctx, ):
 
     await ctx.send(f'{ctx.author.mention} made me say\n{message.content}')
 
-b.run(TOKEN)
+b.run("NzQxNzg0NjcwMzc4NjU1NzY2.GuqQKz.8R3jmDmh3CDjYyzl-O2bl6SLGyhMPqb-tU3jo8")
