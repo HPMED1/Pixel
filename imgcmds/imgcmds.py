a9b4b6de6d8160378c4470d996e71df902482fa9
HPMED1 <62830536+HPMED1@users.noreply.github.com>
2023-06-23 11:15:24 +0300

    gg

--- imgcmds/imgcmds.py
+++ imgcmds/imgcmds.py
@@ -7,15 +7,17 @@ import json
 
 class ImgCmds(app_commands.Group):
   @app_commands.command(name="sadcat")
-  @app_commands.describe(question="question")
-  async def sadcat(self,interaction: discord.Interaction, question: str):
-    data = question.replace(" ", "+")
+  @app_commands.describe(text="text")
+  async def sadcat(self,interaction: discord.Interaction, text: str):
+    data = text.replace(" ", "+")
     r = requests.get(f"https://api.popcat.xyz/sadcat?text={data}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="poohmeme")
@@ -33,11 +35,13 @@ class ImgCmds(app_commands.Group):
   @app_commands.describe(member="member")
   async def jail(self,interaction: discord.Interaction, member: discord.Member):
     r = requests.get(f"https://api.popcat.xyz/jail?image={member.avatar}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="unforgivable")
@@ -45,11 +49,13 @@ class ImgCmds(app_commands.Group):
   async def unforgivable(self,interaction: discord.Interaction, question: str):
     data = question.replace(" ", "+")
     r = requests.get(f"https://api.popcat.xyz/unforgivable?text={data}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="oogway")
@@ -57,29 +63,39 @@ class ImgCmds(app_commands.Group):
   async def oogway(self,interaction: discord.Interaction, question: str):
     data = question.replace(" ", "+")
     r = requests.get(f"https://api.popcat.xyz/oogway?text={data}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="gun")
   @app_commands.describe(member="member")
   async def gun(self,interaction: discord.Interaction, member: discord.Member):
     r = requests.get(f"https://api.popcat.xyz/gun?image={member.avatar}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
-
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
   @app_commands.command(name="randommeme")
   async def randommeme(self,interaction: discord.Interaction):
     r = requests.get("https://api.popcat.xyz/meme")
     rr = r.json()
     await interaction.response.send_message(rr['image'])
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=rr['image'])
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="ship")
@@ -89,32 +105,43 @@ class ImgCmds(app_commands.Group):
     r = requests.get(
       f"https://api.popcat.xyz/ship?user1={member1.avatar}&user2={member2.avatar}"
     )
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this!', file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="clown")
   @app_commands.describe(member="member")
   async def clown(self,interaction: discord.Interaction, member: discord.Member):
     r = requests.get(f"https://api.popcat.xyz/clown?image={member.avatar}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
-
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
   @app_commands.command(name="pet")
   @app_commands.describe(member="member")
   async def pet(self,interaction: discord.Interaction, member: discord.Member):
     r = requests.get(f"https://api.popcat.xyz/pet?image={member.avatar}")
-    with open("image.gif", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.gif"))
+    #with open("image.gif", "wb") as f:
+      #f.write(r.content)
+    #await interaction.response.send_message(
+      #f'{interaction.user.mention} made this meme!',
+      #file=discord.File("image.gif"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="alert")
@@ -122,11 +149,13 @@ class ImgCmds(app_commands.Group):
   async def alert(self,interaction: discord.Interaction, text: str):
     data = text.replace(" ", "+")
     r = requests.get(f"https://api.popcat.xyz/alert?text={data}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 
 
   @app_commands.command(name="facts")
@@ -134,11 +163,12 @@ class ImgCmds(app_commands.Group):
   async def facts(self,interaction: discord.Interaction, text: str):
     data = text.replace(" ", "+")
     r = requests.get(f"https://api.popcat.xyz/facts?text={data}")
-    with open("image.webp", "wb") as f:
-      f.write(r.content)
-    await interaction.response.send_message(
-      f'{interaction.user.mention} made this meme!',
-      file=discord.File("image.webp"))
-
+    embed = discord.Embed(
+            colour=discord.Colour.blurple(),  
+            title=f'{interaction.user.mention} made this meme!'
+        )
+    embed.set_thumbnail(url=interaction.user.avatar)
+    embed.set_image(url=r)
+    await interaction.response.send_message(embed=embed)
 async def setup(bot):
     bot.tree.add_command(ImgCmds(name="memes", description ="commands that generate memes"))
