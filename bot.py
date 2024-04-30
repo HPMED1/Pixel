e5e1f0de396b57e304c5e29249da7e13c9753b92
HPMED <nerohpmed@gmail.com>
2024-04-30 06:33:45 +0300

    modified:   .env     deleted:    __pycache__/settings.cpython-310.pyc     modified:   bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from discord.ext import commands, tasks
 from wand.image import Image
 from wand.drawing import Drawing
 from wand.font import Font
-
+from dotenv import load_dotenv
 
 
 intents = discord.Intents.all()
@@ -39,13 +39,6 @@ async def makeembed(title: str, thumbnail: str):
     embed.set_thumbnail(url=thumbnail)
     return embed
 
-async def makememetosend(title: str, thumbnail: str):
-    embed = discord.Embed(
-        colour=discord.Colour(0xffffff),
-        title=title
-    )
-    embed.set_thumbnail(url=thumbnail)
-    return embed
 
 
 class ImgCmds(app_commands.Group):
@@ -186,30 +179,7 @@ class TxtCmds(app_commands.Group):
                         'list'][random.randrange(0, 10)]['definition'], inline=True)
         await interaction.response.send_message(embed=embed)
 
-    # how much
-    @app_commands.command(name="howmuch", description="returns 0% -> 100%")
-    @app_commands.describe(question="question")
-    async def how_much(self, interaction: discord.Interaction, question: str):
-        question = await getidinfo(question)
-        how_list = [
-            '0%', '1%', '2%', '3%', '4%', '5%', '6%', '7%', '8%', '9%', '10%', '11%',
-            '12%', '13%', '14%', '15%', '16%', '17%', '18%', '19%', '20%', '21%',
-            '22%', '23%', '24%', '25%', '26%', '27%', '28%', '29%', '30%', '31%',
-            '32%', '33%', '34%', '35%', '36%', '37%', '38%', '39%', '40%', '41%',
-            '42%', '43%', '44%', '45%', '46%', '47%', '48%', '49%', '50%', '51%',
-            '52%', '53%', '54%', '55%', '56%', '57%', '58%', '59%', '60%', '61%',
-            '62%', '63%', '64%', '65%', '66%', '67%', '68%', '69%', '70%', '71%',
-            '72%', '73%', '74%', '75%', '76%', '77%', '78%', '79%', '80%', '81%',
-            '82%', '83%', '84%', '85%', '86%', '87%', '88%', '89%', '90%', '91%',
-            '92%', '93%', '94%', '95%', '96%', '97%', '98%', '99%', '100%', 'idk'
-        ]
-        embed = discord.Embed(
-            colour=discord.Colour(0xffffff),
-            title=f'how much {question}'
-        )
-        embed.add_field(name="Answer", value=f'{random.choice(how_list)}')
 
-        await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="showerthought", description="some shower thoughts so you don't have to think in the shower")
     async def showerthought(self, interaction: discord.Interaction):
@@ -243,5 +213,7 @@ async def on_ready():
     print('Command tree synced.')
     # print "ready" in the console when the bot is ready to work
     print("ready")
+load_dotenv()  # Load .env file
+b.run(os.getenv('DISCORD_API_TOKEN'))
 
-b.run("NzQxNzg0NjcwMzc4NjU1NzY2.Gthf00.itpAHdxTKLd5MybCLFLiBhZPpja97MRZjO5egI")
+
